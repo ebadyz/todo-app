9448de7e8cd826ebad26e270e063ed6a1df2e5bf
ebadyz <ebad.yousefzade@gmail.com>
2022-07-21 15:03:14 +0430

    make List component more customized

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,18 +2,37 @@ import { List as ChakraList, ListItem } from "@chakra-ui/react";
 
 interface Item {
   id: string;
-  projectName: string;
 }
+
+type FlexDirection =
+  | "column"
+  | "inherit"
+  | "-moz-initial"
+  | "initial"
+  | "revert"
+  | "unset"
+  | "column-reverse"
+  | "row"
+  | "row-reverse"
+  | undefined;
 
 interface ListProps<T> {
   items: T[];
+  renderItem: (item: T) => React.ReactNode;
+  flexDir?: FlexDirection;
+  gap?: number;
 }
 
-function List<T extends Item>({ items }: ListProps<T>): JSX.Element {
+function List<T extends Item>({
+  items,
+  renderItem,
+  flexDir = "row",
+  gap,
+}: ListProps<T>) {
   return (
-    <ChakraList>
+    <ChakraList display="flex" flexDir={flexDir} gap={gap}>
       {items.map((item) => (
-        <ListItem key={item.id}>{item.projectName}</ListItem>
+        <ListItem key={item.id}>{renderItem(item)}</ListItem>
       ))}
     </ChakraList>
   );
