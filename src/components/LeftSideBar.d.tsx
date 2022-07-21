d858d1b1faf64414b4abf7554ae703d30c978000
ebadyz <ebad.yousefzade@gmail.com>
2022-07-21 15:05:10 +0430

    feature: project color

--- src/components/LeftSideBar.d.tsx
+++ src/components/LeftSideBar.d.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Button, HStack, Input } from "@chakra-ui/react";
+import { Box, Button, Circle, HStack, Input, VStack } from "@chakra-ui/react";
 import { useDisclosure, ModalFooter, ModalBody } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
 import Modal from "./Modal";
@@ -8,46 +8,103 @@ import List from "./List";
 interface Project {
   id: string;
   projectName: string;
+  projectColor: string;
 }
 
 type ProjectName = string;
+type ProjectColor = string;
 type Projects = Project[];
+
+const COLORS_SCHEMA = [
+  { id: uuidv4(), color: "#FFE11C" },
+  { id: uuidv4(), color: "#2ECC71" },
+  { id: uuidv4(), color: "#FF38AD" },
+  { id: uuidv4(), color: "#01C3FF" },
+  { id: uuidv4(), color: "#941B80" },
+  { id: uuidv4(), color: "#3C4B6D" },
+  { id: uuidv4(), color: "#F93852" },
+];
 
 function LeftSideBar(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [projectName, setProjectName] = useState<ProjectName>("");
+  const [projectColor, setProjectColor] = useState<ProjectColor>("");
   const [projects, setProjects] = useState<Projects>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setProjectName(e.target.value);
   };
 
+  const handleSelect = (color: string): void => {
+    setProjectColor(color);
+  };
+
   const handleSubmit = (
     e: React.FormEvent<HTMLFormElement>,
-    name: string
+    name: string,
+    color: string
   ): void => {
     e.preventDefault();
-    setProjects((prev) => [...prev, { id: uuidv4(), projectName: name }]);
+    const randomIndex = Math.floor(
+      Math.random() * (COLORS_SCHEMA.length - 1 - 0)
+    );
+    const randomColorSchema = COLORS_SCHEMA[randomIndex];
+    const { color: randomProjectColor } = randomColorSchema;
+
+    setProjects((prev) => [
+      ...prev,
+      {
+        id: uuidv4(),
+        projectName: name,
+        projectColor: color ? color : randomProjectColor,
+      },
+    ]);
     setProjectName("");
+    setProjectColor("");
     onClose();
   };
 
   return (
     <Box as="aside" w="33%" border="1px" borderColor="blue" position="relative">
-      {projects.length > 0 && <List items={projects} />}
-      <Button position="absolute" bottom="0" onClick={onOpen}>
+      {projects.length > 0 && (
+        <List
+          flexDir="column"
+          items={projects}
+          renderItem={(item) => (
+            <HStack>
+              <Circle size={6} bg={item.projectColor} />
+              <p>{item.projectName}</p>
+            </HStack>
+          )}
+        />
+      )}
+      <Button position="absolute" bottom="0" onClick={onOpen} w="100%">
         Add project
       </Button>
       <Modal isOpen={isOpen} onClose={onClose} title="Add Project">
-        <form onSubmit={(e) => handleSubmit(e, projectName)}>
+        <form onSubmit={(e) => handleSubmit(e, projectName, projectColor)}>
           <ModalBody>
-            <Input
-              type="text"
-              placeholder="New Project Name"
-              value={projectName}
-              onChange={handleChange}
-              autoFocus
-            />
+            <VStack>
+              <Input
+                type="text"
+                placeholder="New Project Name"
+                value={projectName}
+                onChange={handleChange}
+                autoFocus
+              />
+              <List
+                items={COLORS_SCHEMA}
+                renderItem={(item) => (
+                  <Circle
+                    cursor="pointer"
+                    size={6}
+                    bg={item.color}
+                    onClick={() => handleSelect(item.color)}
+                  />
+                )}
+                gap={6}
+              />
+            </VStack>
           </ModalBody>
           <ModalFooter>
             <HStack>
