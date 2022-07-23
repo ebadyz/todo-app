f99368416eb7a4ebf2b4e0572fbc41966e2c529a
ebadyz <ebad.yousefzade@gmail.com>
2022-07-23 19:16:23 +0430

    completed submit handler of subtask form and show subtasks

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,68 @@ import {
   InputLeftElement,
   InputRightElement,
   VStack,
+  Box,
+  Text,
 } from "@chakra-ui/react";
 import Layout from "./components/Layout";
-import LeftSideBar from "./components/LeftSideBar.d";
+import LeftSideBar from "./components/LeftSideBar";
 import { Project } from "./types";
 import theme from "./components/Theme";
 import { AddIcon } from "@chakra-ui/icons";
 import List from "./components/List";
+import { v4 as uuidv4 } from "uuid";
 
+type Projects = Project[];
 type SubTaskTitle = string;
 
 function App(): JSX.Element {
+  const [projects, setProjects] = useState<Projects>([]);
   const [project, setProject] = useState<Project | null>(null);
   const [subTaskTitle, setSubTaskTitle] = useState<SubTaskTitle>("");
 
-  const getProject = (project: Project): void => {
+  const handleSetSelectedProject = (project: Project): void => {
     setProject(project);
   };
 
+  const handleSetProjects = (project: Project): void => {
+    setProjects((prev) => [...prev, project]);
+  };
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    setSubTaskTitle(e.target.value.trim());
+    setSubTaskTitle(e.target.value);
   };
 
   const handleCreateSubTask = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    //! TODO: Refactor finding a selected project and add a subtask
+    if (projects.length > 0 && project) {
+      const selectedProject = projects.find((item) => item.id === project.id);
+      if (selectedProject) {
+        selectedProject.subTasks?.push({
+          id: uuidv4(),
+          title: subTaskTitle.trim(),
+        });
+      }
+    }
+    setSubTaskTitle("");
   };
 
   return (
     <ChakraProvider theme={theme}>
-      <Layout leftSideBar={<LeftSideBar getProject={getProject} />}>
+      <Layout
+        leftSideBar={
+          <LeftSideBar
+            projects={projects}
+            handleSetProjects={handleSetProjects}
+            handleSetSelectedProject={handleSetSelectedProject}
+          />
+        }
+      >
         {project && Object.values(project).length > 0 && (
-          <VStack alignItems="flex-start">
-            <Heading>{project.projectName}</Heading>
+          <VStack alignItems="flex-start" spacing={10} overflow="hidden">
+            <Heading as="h1" size="xl">
+              {project.projectName}
+            </Heading>
             <form
               style={{ width: "100%" }}
               onSubmit={(e) => handleCreateSubTask(e)}
@@ -63,7 +93,23 @@ function App(): JSX.Element {
             {project.subTasks && (
               <List
                 items={project.subTasks}
-                renderItem={(item) => item.title}
+                renderItem={(item) => (
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    px="4"
+                    bg="whiteAlpha.100"
+                    h={12}
+                  >
+                    <Text>{item.title}</Text>
+                  </Box>
+                )}
+                display="flex"
+                flexDir="column"
+                h="430px"
+                overflow="auto"
+                gap="3"
+                w="100%"
               />
             )}
           </VStack>
