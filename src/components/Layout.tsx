a5bc26cdaaad7b29f7961b3a8ee458f542460a06
ebadyz <ebad.yousefzade@gmail.com>
2022-07-25 21:11:30 +0430

    updated Layout for showing rightSideBar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,27 +3,34 @@ import { Box, Heading } from "@chakra-ui/react";
 interface ILayoutProps {
   children: React.ReactNode;
   leftSideBar: React.ReactElement;
+  rightSideBar: React.ReactElement;
+  handleSetToggle: (newState: boolean) => void;
 }
 
-function Layout({ children, leftSideBar }: ILayoutProps): JSX.Element {
+function Layout({
+  children,
+  leftSideBar,
+  rightSideBar,
+  handleSetToggle,
+}: ILayoutProps): JSX.Element {
   return (
     <Box display="grid" placeItems="center" h="100vh">
       <header>
         <Heading>TODO App</Heading>
       </header>
       <Box
+        as="main"
         display="flex"
         border="1px"
         w="90%"
         h="90vh"
         borderColor="whiteAlpha.600"
         mx="auto"
+        onClick={() => handleSetToggle(false)}
       >
         {leftSideBar}
-        <Box as="main" p="4" w="70%">
-          {children}
-        </Box>
-        {/* <RightSideBar /> */}
+        {children}
+        {rightSideBar}
       </Box>
     </Box>
   );
